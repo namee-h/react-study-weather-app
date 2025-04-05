a5534a0f8c49120c57a0882b1dc21e8fa0d734e9
Sara han <hans63237@gmail.com>
2025-04-06 00:01:01 +0900

    풍속 강수량 습도 정보 추가

--- src/component/WeatherBox.jsx
+++ src/component/WeatherBox.jsx
@@ -2,15 +2,28 @@ import React from "react";
 
 const WeatherBox = ({weather,loading}) => {
   
-  // console.log("weather?",weather)  
+  console.log("weather?",weather)  
   return <div className="weather-box">
     <div className={`weather-box-info ${loading?"hidden":""}`}>
-      <div className="location-name">{weather?.location.name}</div>
-      <div>{weather?.current.temp_c} ℃</div>
-      <div>{weather?.current.temp_f} ℉</div>
+      <h4>{weather?.location.name}</h4>
+      <h2>{weather?.current.temp_c} <span>℃</span> / {weather?.current.temp_f} <span>℉</span></h2>
       <div className="weather-conditions">
         <img src={weather?.current.condition.icon}/>
         <div>{weather?.current.condition.text}</div>
+      </div>
+      <div className="wind-area">
+        <div>
+          <div>풍속</div>
+          <h3>{weather?.current.wind_kph}<span> kph</span></h3>
+        </div>
+        <div>
+          <div>강수량</div>
+          <h3>{weather?.current.precip_mm}<span> mm</span></h3>
+        </div>
+        <div>
+          <div>습도</div>
+          <h3>{weather?.current.wind_kph}<span> %</span></h3>
+        </div>
       </div>
     </div>
   </div>;
